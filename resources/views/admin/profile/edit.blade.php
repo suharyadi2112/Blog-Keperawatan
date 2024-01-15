7688e8175b48b5662e9ad23f04eb4d38a3ca8c24
MSI\USER <yogafebrianmaulana12@gmail.com>
2024-01-15 10:30:26 +0700

    no message

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>Edit User</h1>
 @stop
 
 @section('content')
@@ -14,7 +14,7 @@
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Bordered Table</h3>
+                            <h3 class="card-title">Data User</h3>
                         </div>
                         <!-- /.card-header -->
                         <div class="card-body">
