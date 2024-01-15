b8bae4283923e2cd3ae6d71114eb506829a4c176
ximc <idrcorner@gmail.com>
2024-01-15 15:20:40 +0700

    dokumen crud

--- resources/views/admin/document/form_create.blade.php
+++ resources/views/admin/document/form_create.blade.php
@@ -12,33 +12,52 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-6">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Tambah Dokumen</h3>
+                    <form action="{{ route('dokumen.store') }}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-title">Tambah Dokumen</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="namaDokumen">Nama Dokumen</label>
+                                    @error('namaDokumen')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                    <input type="text" class="form-control" id="namaDokumen" name="namaDokumen"
+                                        placeholder="Nama Dokumen" value="{{ old('namaDokumen') }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="deskripsi">Deskripsi</label>
+                                    @error('deskripsi')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                    <textarea class="form-control" id="deskripsi" name="deskripsi" placeholder="Deskripsi"> {{ old('deskripsi') }}</textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputFile">File</label>
+                                    @error('file')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="exampleInputFile"
+                                                name="file">
+                                            <label class="custom-file-label" for="exampleInputFile">Pilih file</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer ">
 
+                                <button type="submit" class="btn btn-primary float-right">Simpan</button>
+                                <a href="{{ route('dokumen.index') }}" class="btn btn-secondary float-right mr-2">Batal</a>
+                            </div>
                         </div>
-                        <!-- /.card-header -->
-                        <div class="card-body">
-                            <div class="form-group">
-                                <label for="">Nama Dokumen</label>
-                                <input type="text" id="" name="namaDokumen" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="">Deskripsi</label>
-                                <textarea id="" name="deskripsi" class="form-control"></textarea>
-                            </div>
-
-                            <div class="form-group">
-                                {{-- <label for="formFile">Upload</label> --}}
-                                <input class=" " type="file" id="formFile">
-                            </div>
-
-                            <div class="form-group mt-4 float-right">
-                                <a href="{{ route('dokumen.index') }}" class="btn  btn-secondary">Batal</a>
-                                <button type="submit" class="btn btn-primary">Tambah</button>
-                            </div>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
--- resources/views/admin/document/form_edit.blade.php
+++ resources/views/admin/document/form_edit.blade.php
@@ -7,5 +7,64 @@
 @stop
 
 @section('content')
-    Form Edit
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <form action="{{ route('dokumen.update', $dokumen->id) }}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        @method('PATCH')
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="card-title">Ubah Dokumen</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="nama">Nama Dokumen</label>
+                                    @error('nama')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                    <input type="text" class="form-control" id="nama" name="nama"
+                                        placeholder="Nama Dokumen" value="{{ old('nama') ? old('nama') : $dokumen->nama }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="deskripsi">Deskripsi</label>
+                                    @error('deskripsi')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                    <textarea class="form-control" id="deskripsi" name="deskripsi" placeholder="Deskripsi"> {{ old('deskripsi') ? old('deskripsi') : $dokumen->deskripsi }}</textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputFile">File</label>
+                                    @error('file')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                    <br /> <small class="text-primary"><i
+                                            class="fas fa-file mr-1"></i>{{ str_replace('dokumen/', '', $dokumen->file) }}</small>
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="exampleInputFile"
+                                                name="file">
+                                            <label class="custom-file-label" for="exampleInputFile">Pilih file</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer ">
+
+                                <button type="submit" class="btn btn-primary float-right">Ubah</button>
+                                <a href="{{ route('dokumen.index') }}" class="btn btn-secondary float-right mr-2">Batal</a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
 @endsection
