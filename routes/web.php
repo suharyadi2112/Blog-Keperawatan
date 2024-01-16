44ffb1a1453ae6ba0f2f05f38a6f9adc5e1628bc
Indra Kurniawan <67799408+indrakuu@users.noreply.github.com>
2024-01-16 10:52:36 +0700

    update double data

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,15 @@ use App\Http\Controllers\DocumentController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
 
-Auth::routes();
+Auth::routes(
+    [
+        'register' => true,
+        'reset' => false,
+        'verify' => false,
+    ]
+);
 
 Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
@@ -27,13 +33,6 @@ Route::post('/dokumentasi', [App\Http\Controllers\DokumentasiController::class, 
 Route::get('/dokumentasi/{id}', [App\Http\Controllers\DokumentasiController::class, 'show'])->name('dokumentasi.show');
 Route::delete('/dokumentasi/{id}', [App\Http\Controllers\DokumentasiController::class, 'destroy'])->name('dokumentasi.destroy');
 
-
-
-
-
-
-
-Route::get('/dokumentasi', [App\Http\Controllers\DokumentasiController::class, 'index'])->name('dokumentasi');
 
 // Document Route
 Route::resource('dokumen', DocumentController::class);
@@ -45,6 +44,6 @@ Route::delete('/delInformasi/{id}', [App\Http\Controllers\InformasiController::c
 Route::get('/informasi/{id}', [App\Http\Controllers\InformasiController::class, 'informasiByID'])->name('informasiByID');
 
 
-Route::get('/profile/index', [App\Http\Controllers\UserController::class, 'index'])->name('profile');
+Route::get('/profile', [App\Http\Controllers\UserController::class, 'index'])->name('profile');
 Route::get('/profile/edit/{id}', [App\Http\Controllers\UserController::class, 'edit'])->name('editprofile');
 Route::post('/profile/update', [App\Http\Controllers\UserController::class, 'update'])->name('updateprofile');
