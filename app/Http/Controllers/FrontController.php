522bb94cbab98ab618f9eae978018dc5426e2ca7
Indra Kurniawan <67799408+indrakuu@users.noreply.github.com>
2024-01-17 10:03:07 +0700

    membuat halaman dokumentasi

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -9,7 +9,10 @@ class FrontController extends Controller
     //
     public function indexfrontend()
     {
-
         return view('frontend.index');
+    }
+
+    public function indexDokumentasi(){
+        return view('frontend.dokumentasi');
     }
 }
