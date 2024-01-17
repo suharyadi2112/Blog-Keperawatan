7bed045f56b2c5f0e65b2468d4e01ac6fa2a339d
MSI\USER <yogafebrianmaulana12@gmail.com>
2024-01-17 10:15:59 +0700

    no message

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Informasi;
 
 class FrontController extends Controller
 {
     //
     public function indexfrontend()
     {
-
+       
         return view('frontend.index');
+
     }
 }
