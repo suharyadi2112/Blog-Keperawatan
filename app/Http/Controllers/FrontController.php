51c985d5734cdedb63b4026ada6d6a27e02ee9bd
ximc <idrcorner@gmail.com>
2024-01-17 10:02:16 +0700

    front controller index, add

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Models\DokumenModel;
+use App\Models\DokumenModel as ModelsDokumenModel;
 
 class FrontController extends Controller
 {
     //
     public function indexfrontend()
     {
+        $dokumens=ModelsDokumenModel::orderby('created_at','desc')->limit(5)->get();
 
-        return view('frontend.index');
+
+        return view('frontend.index')->with('dokumens',$dokumens);
     }
 }
