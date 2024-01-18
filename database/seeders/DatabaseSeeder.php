d6a69fed4fc29c2ad667c497c7c2f8210ebd59da
MSI\USER <yogafebrianmaulana12@gmail.com>
2024-01-18 10:51:49 +0700

    fixing seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,16 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        //     'password' =>  Hash::make('12345678'),
-        // ]);
-        
-        // create 1 user
-        \App\Models\User::factory()->create([
+        \App\Models\User::create([
             'name' => 'Admin Keperawatan',
             'username' => 'admkp',
             'password' =>  Hash::make('12345678'),
