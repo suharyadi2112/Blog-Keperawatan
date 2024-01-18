683798864e6f74d118b3343863a0ca9874cd995a
MSI\USER <yogafebrianmaulana12@gmail.com>
2024-01-18 14:15:09 +0700

    fixing

--- resources/views/frontend/informasi.blade.php
+++ resources/views/frontend/informasi.blade.php
@@ -2,91 +2,94 @@
 @section('title', 'Informasi')
 @section('content')
 
-<section class="blog grid section" id="blog" style="padding-top: 0px;">
-    <div class="breadcrumbs overlay" style="background-image: url('{{ asset('assets/img/rsup.png') }}');">
-        <div class="container">
-            <div class="bread-inner">
-                <div class="row">
-                    <div class="col-12">
-                        <h2>Informasi</h2>
-                        <ul class="bread-list">
-                        <li><a href="{{route('welcome')}}">Home</a></li>
-                        <li><i class="icofont-simple-right"></i></li>
-                        <li class="active">Informasi</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    
-    <div class="container">
-        <div class="row">
-            @if (count($informasi) > 0)
-            @foreach ($informasi as $informasi)
-            <div class="col-lg-4 col-md-6 col-12">
-                <div class="single-news">
-                    <div class="news-head lightbox">
-                        <img src="{{ asset('storage/thumbnail/' . $informasi->thumbnail) }}" alt="#" style="width: 100%; height: 200px; object-fit: cover;">
-                    </div>
-                    <div class="news-body">
-                        <div class="news-content">
-                            <div class="date">
-                                {{ \Carbon\Carbon::parse($informasi->created_at)->format('d M Y') }}</div>
-                            {{-- <h2><a href="{{ Route("informasiDetail", ['id' => $informasi->id] ) }}" target="_blank">{!! $informasi->judul_informasi !!}</a></h2> --}}
-                            <h2><a href="#" onclick="openInformasiDetail({{ $informasi->id }}); return false;">{!! $informasi->judul_informasi !!}</a></h2>
-                            <p class="text">{!! substr(strip_tags($informasi->isi_informasi), 0, 200) !!}...</p>
+    <section class="blog grid section" id="blog" style="padding-top: 0px;">
+        <div class="breadcrumbs overlay" style="background-image: url('{{ asset('assets/img/rsup.png') }}');">
+            <div class="container">
+                <div class="bread-inner">
+                    <div class="row">
+                        <div class="col-12">
+                            <h2>Informasi</h2>
+                            <ul class="bread-list">
+                                <li><a href="{{ route('welcome') }}">Home</a></li>
+                                <li><i class="icofont-simple-right"></i></li>
+                                <li class="active">Informasi</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
-            @endforeach
-            <div class="col-12 d-flex justify-content-center">
-                <div class="pagination">
-                    {!! $informasis->links() !!}
-                </div>
-            </div>
-            @else
-            <div class="col-12 d-flex justify-content-center">
-                <p>Saat ini belum ada informasi</p>
-            </div>
-            @endif
         </div>
-    </div>
-</section>
 
-<style scoped>
-     .breadcrumbs.overlay {
-        background-size: cover;
-        background-position: center center;
-        background-repeat: no-repeat;
-    }
-</style>
+        <div class="container">
+            <div class="row">
+                @if (count($informasi) > 0)
+                    @foreach ($informasi as $informasi)
+                        <div class="col-lg-4 col-md-6 col-12">
+                            <div class="single-news">
+                                <div class="news-head lightbox">
+                                    <img src="{{ asset('storage/thumbnail/' . $informasi->thumbnail) }}" alt="#"
+                                        style="width: 100%; height: 200px; object-fit: cover;">
+                                </div>
+                                <div class="news-body">
+                                    <div class="news-content">
+                                        <div class="date">
+                                            {{ \Carbon\Carbon::parse($informasi->created_at)->format('d M Y') }}</div>
+                                        {{-- <h2><a href="{{ Route("informasiDetail", ['id' => $informasi->id] ) }}" target="_blank">{!! $informasi->judul_informasi !!}</a></h2> --}}
+                                        <h2><a href="#"
+                                                onclick="openInformasiDetail({{ $informasi->id }}); return false;">{!! $informasi->judul_informasi !!}</a>
+                                        </h2>
+                                        <p class="text">{!! substr(strip_tags($informasi->isi_informasi), 0, 200) !!}...</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                    <div class="col-12 d-flex justify-content-center">
+                        <div class="pagination">
+                            {!! $informasis->links() !!}
+                        </div>
+                    </div>
+                @else
+                    <div class="col-12 d-flex justify-content-center">
+                        <p>Saat ini belum ada informasi</p>
+                    </div>
+                @endif
+            </div>
+        </div>
+    </section>
 
-<script type="text/javascript">
-     function openInformasiDetail(id) {
-        var form = document.createElement('form');
-        form.method = 'POST';
-        form.action = '{{ Route('informasiDetail') }}'; 
-        form.target = '_blank';
+    <style scoped>
+        .breadcrumbs.overlay {
+            background-size: cover;
+            background-position: center center;
+            background-repeat: no-repeat;
+        }
+    </style>
 
-        var idInput = document.createElement('input');
-        idInput.type = 'hidden';
-        idInput.name = 'id';
-        idInput.value = id;
-        form.appendChild(idInput);
+    <script type="text/javascript">
+        function openInformasiDetail(id) {
+            var form = document.createElement('form');
+            form.method = 'POST';
+            form.action = '{{ Route('informasiDetail') }}';
+            form.target = '_blank';
 
-        // CSRF
-        var csrfTokenInput = document.createElement('input');
-        csrfTokenInput.type = 'hidden';
-        csrfTokenInput.name = '_token';
-        csrfTokenInput.value = '{{ csrf_token() }}';
-        form.appendChild(csrfTokenInput);
+            var idInput = document.createElement('input');
+            idInput.type = 'hidden';
+            idInput.name = 'id';
+            idInput.value = id;
+            form.appendChild(idInput);
 
-        // Submit the form
-        document.body.appendChild(form);
-        form.submit();
-    }
-</script>
+            // CSRF
+            var csrfTokenInput = document.createElement('input');
+            csrfTokenInput.type = 'hidden';
+            csrfTokenInput.name = '_token';
+            csrfTokenInput.value = '{{ csrf_token() }}';
+            form.appendChild(csrfTokenInput);
+
+            // Submit the form
+            document.body.appendChild(form);
+            form.submit();
+        }
+    </script>
 
 @endsection
