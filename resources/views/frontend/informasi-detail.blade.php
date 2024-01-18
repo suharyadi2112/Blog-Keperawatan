a644ed15afe2412d2f086618bc065e2c7dbfa4d3
suharyadi <yadisuhar10@gmail.com>
2024-01-18 08:55:53 +0700

    - informasi dashboard bukan detail

--- resources/views/frontend/informasi-detail.blade.php
+++ resources/views/frontend/informasi-detail.blade.php
@@ -8,7 +8,7 @@
             <div class="bread-inner">
                 <div class="row">
                     <div class="col-12">
-                        <h2>Informasi</h2>
+                        <h2>Blog Single</h2>
                         <ul class="bread-list">
                         <li><a href="index.html">Home</a></li>
                         <li><i class="icofont-simple-right"></i></li>
@@ -20,45 +20,48 @@
         </div>
     </div>
     
+   
     <div class="container">
+            
         <div class="row">
-            @if (count($informasi) > 0)
-            @foreach ($informasi as $informasi)
+            <div class="col-lg-12">
+                <div class="section-title">
+                    <h2>Dokumentasi</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
             <div class="col-lg-4 col-md-6 col-12">
                 <div class="single-news">
                     <div class="news-head lightbox">
-                        <img src="https://via.placeholder.com/560x370" alt="#" style="width: 100%; height: 200px; object-fit: cover;">
+                        <img src="#" alt="#" style="width: 100%; height: 200px; object-fit: cover;">
                     </div>
                     <div class="news-body">
                         <div class="news-content">
-                            <div class="date">
-                                {{ \Carbon\Carbon::parse($informasi->created_at)->format('d M Y') }}</div>
-                            <h2><a href="{{ Route("informasiDetail", ['id' => $informasi->id] ) }}" target="_blank">{!! $informasi->judul_informasi !!}</a></h2>
-                            <p class="text">{!! substr(strip_tags($informasi->isi_informasi), 0, 200) !!}...</p>
+                            <h2>tes</h2>
                         </div>
                     </div>
                 </div>
             </div>
-            @endforeach
             <div class="col-12 d-flex justify-content-center">
                 <div class="pagination">
-                    {!! $informasis->links() !!}
+                    {{-- {!! $dokumentasis->links() !!} --}}
                 </div>
             </div>
-            @else
+            {{-- @else
             <div class="col-12 d-flex justify-content-center">
                 <p>Saat ini belum ada dokumentasi</p>
             </div>
-            @endif
+            @endif --}}
         </div>
     </div>
 </section>
 
 <style scoped>
      .breadcrumbs.overlay {
-        background-size: cover;
-        background-position: center center;
-        background-repeat: no-repeat;
+        background-size: cover; /* Adjust as needed */
+        background-position: center center; /* Adjust as needed */
+        background-repeat: no-repeat; /* Adjust as needed */
     }
 </style>
 
